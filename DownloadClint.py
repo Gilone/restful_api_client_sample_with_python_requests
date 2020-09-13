e164db9a368bc0ba0d00618f2c1581a6e81fd2f3
fehao <hao.feng@nokia-sbell.com>
2020-09-13 16:22:34 +0800

    update

---
+++ DownloadClint.py
@@ -0,0 +1,30 @@
+import requests
+import os
+from requests.auth import HTTPBasicAuth
+
+class Clint():
+    def __init__(self, account, password, file_name, download_path):
+        self.account = account
+        self.paasword = password
+        self.file_name = file_name
+        self.download_path = download_path
+
+    def _make_dir(self, path):
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+    def download(self):
+        url = 'http://localhost:8800/helloweb/download/' + self.file_name
+        self._make_dir(self.download_path)
+        resp = requests.get(url,auth=HTTPBasicAuth(self.account,self.paasword))
+        with open(self.download_path+self.file_name,"wb") as download_file:
+            download_file.write(resp.content)
+
+
+if __name__ == "__main__":
+    ACCOUNT = 'usr'
+    PASSWORD = '123'
+    FILENAME = 'tests.xlsx'
+    DOWNLOADPATH = './downloaded/'
+    clint = Clint(ACCOUNT, PASSWORD, FILENAME, DOWNLOADPATH)
+    clint.download()
